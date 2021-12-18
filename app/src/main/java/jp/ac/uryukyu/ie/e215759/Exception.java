a78502f384f705811a4be995a597c76a8c42e7ec
tamotsu <e215759@ie.u-ryukyu.ac.jp>
2021-12-18 18:01:34 +0900

    Report5_step2

--- app/src/main/java/jp/ac/uryukyu/ie/e215759/Exception.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e215759/Exception.java
@@ -1,8 +1,13 @@
 package jp.ac.uryukyu.ie.e215759;
 
 public class Exception {
-    public static void main(String[] main){
-    String str = null;
-    System.out.println(str.length());
+    public static void main(String[] args){
+        String str = null;
+        try{
+            System.out.println(str.length());
+        } catch (NullPointerException e) {
+            System.out.println("エラー：NullPointerException");
+            System.out.println(e.getMessage());
+        }
     }
 }
