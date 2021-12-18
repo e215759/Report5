e243b0e82c8aeffe5bced1562a5ac8a712b03e2d
tamotsu <e215759@ie.u-ryukyu.ac.jp>
2021-12-18 18:48:25 +0900

    Report5_step3

--- app/src/main/java/jp/ac/uryukyu/ie/e215759/Exception.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e215759/Exception.java
@@ -1,5 +1,6 @@
 package jp.ac.uryukyu.ie.e215759;
 
+/*
 public class Exception {
     public static void main(String[] args){
         String str = null;
@@ -7,6 +8,19 @@ public class Exception {
             System.out.println(str.length());
         } catch (NullPointerException e) {
             System.out.println("エラー：NullPointerException");
+            System.out.println(e.getMessage());
+        }
+    }
+}
+*/
+
+public class Exception{
+    public static void main(String[] args){
+        String str = "百二十三";
+        int value = Integer.parseInt(str);
+        try{
+        } catch (NumberFormatException e) {
+            System.out.println(value);
             System.out.println(e.getMessage());
         }
     }
